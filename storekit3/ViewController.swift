69e924045c20a58f126a30afe28c7ea8ba469f61
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-07-26 13:56:39 +0900

    firstCommit

--- storekit3/ViewController.swift
+++ storekit3/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let purchaseButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("課金", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        button.addTarget(self, action: #selector(purchaseButtonPressid), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        purchaseButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(purchaseButton)
+        
+        let purchaseButtonConstraints = [
+            purchaseButton.heightAnchor.constraint(equalToConstant: 50),
+            purchaseButton.widthAnchor.constraint(equalToConstant: 200),
+            purchaseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            purchaseButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ]
+        
+        NSLayoutConstraint.activate(purchaseButtonConstraints)
+        
+    }
+    
+    @objc func purchaseButtonPressid() {
+        
     }
 
 
